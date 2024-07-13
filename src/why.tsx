b21c0e044c4dc9b90819e77e567fd635d544c0dc
Curtis Fenner <3289892+CurtisFenner@users.noreply.github.com>
2024-07-13 13:49:22 +0900

    bar graphs

--- src/why.tsx
+++ src/why.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import { SwatchButtonRow } from "./components/swatch";
 import { ColorChip } from "./components/color-chip";
+import { binarySearchNearest } from "./util";
 
 function generateChromaDiscrepancies(): {
 	chromaErrorRatio: number;
@@ -112,11 +113,144 @@ function HslChromaDiscrepancies() {
 	</section>
 }
 
+type ColorBar = {
+	name: string,
+	percent: (c: culori.Color) => number,
+	label: (n: number) => string,
+};
+
+const oklchColorBar: ColorBar = {
+	name: "OKLCH",
+	percent: c => culori.oklch(c).c,
+	label: x => x.toFixed(2),
+};
+const hslColorBar: ColorBar = {
+	name: "HSL",
+	percent: c => culori.hsl(c).s * 100,
+	label: x => (x * 100).toFixed(0) + "%",
+};
+
+function TheColors(props: {
+	palette: culori.Color[],
+	bars: ColorBar[],
+}) {
+	const palette = props.palette;
+	const bars = props.bars;
+
+	const barMaxes = bars.map(bar => Math.max(...palette.map(color => bar.percent(color))));
+
+	return <table style={{ width: "100%" }}>
+		<tbody>
+			<tr>
+				{palette.map(color => {
+					return bars.map((bar, barI) => {
+						return <td key={barI} style={{ position: "relative", height: "4rem" }}>
+							<div style={{
+								position: "relative",
+								left: 0,
+								top: 0,
+								width: "100%",
+								height: "100%",
+							}}>
+								<div style={{
+									position: "absolute",
+									backgroundColor: culori.formatHex(color),
+									bottom: 0,
+									width: "100%",
+									height: (bar.percent(color) / barMaxes[barI] * 100).toFixed(1) + "%",
+								}}></div>
+								<div style={{
+									position: "absolute",
+									fontSize: "80%",
+									left: "50%",
+									top: "50%",
+									transform: "translate(-50%, -50%)",
+									textAlign: "right",
+								}}>{bar.name}</div>
+							</div>
+						</td>
+					});
+				})}
+			</tr>
+			<tr>
+				{palette.map((color, paletteI) => {
+					return <td key={paletteI} colSpan={bars.length}
+						style={{
+							background: culori.formatRgb(color),
+						}}>
+						<div style={{
+							width: "5em",
+							height: "2em",
+							lineHeight: "2em",
+							textAlign: "center",
+							position: "relative",
+						}}>
+							{culori.formatHex(color)}
+						</div>
+					</td>
+				})}
+			</tr>
+		</tbody>
+	</table >
+}
+
+const consistentChromaPalette: culori.Oklch[] = [];
+const darkConsistentChromaPalette: culori.Oklch[] = [];
+for (let hue = 0; hue < 360; hue += 40) {
+	consistentChromaPalette.push({
+		mode: "oklch",
+		h: hue + 23,
+		c: 0.1229,
+		l: 0.7285,
+	});
+
+	darkConsistentChromaPalette.push({
+		mode: "oklch",
+		h: hue + 23,
+		c: 0.08,
+		l: 0.47,
+	});
+}
+
+const consistentSaturationPalette: culori.Color[] = consistentChromaPalette.map(base => {
+	const targetSaturation = 0.45;
+	// Vary the chroma to get the desired HSL saturation.
+	const bestChroma = binarySearchNearest(base.c, guessChroma => {
+		if (guessChroma < 0 || guessChroma > 1) return Infinity;
+		const withNewChroma = culori.clampChroma({ ...base, c: guessChroma }, "oklch", "rgb");
+		return Math.abs(culori.hsl(withNewChroma).s - targetSaturation);
+	}, 1);
+	const actual = culori.clampChroma({ ...base, c: bestChroma }, "oklch", "rgb");
+	return actual;
+});
+
+const darkConsistentSaturationPalette: culori.Color[] = darkConsistentChromaPalette.map(base => {
+	const targetSaturation = 0.7;
+	// Vary the chroma to get the desired HSL saturation.
+	const bestChroma = binarySearchNearest(base.c, guessChroma => {
+		if (guessChroma < 0 || guessChroma > 1) return Infinity;
+		const withNewChroma = culori.clampChroma({ ...base, c: guessChroma }, "oklch", "rgb");
+		return Math.abs(culori.hsl(withNewChroma).s - targetSaturation);
+	}, 1);
+	const actual = culori.clampChroma({ ...base, c: bestChroma }, "oklch", "rgb");
+	return actual;
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		Why use OKLCH?<br />
 		OKLCH makes it easier to <b>find</b> and <b>describe</b> the colors you
 		want.
+		<hr />
+		These colors have a consistent <b>saturation</b> in HSL. The size of the
+		bars indicates each color's relative <em>chroma</em> in OKLCH.
+		<TheColors palette={consistentSaturationPalette} bars={[oklchColorBar]} />
+		<TheColors palette={darkConsistentSaturationPalette} bars={[oklchColorBar]} />
+		<hr />
+		These colors have a consistent <b>chroma</b> in OKLCH. The size of the
+		bars indicates each color's relative <em>saturation</em> in HSL.
+		<TheColors palette={consistentChromaPalette} bars={[hslColorBar]} />
+		<TheColors palette={darkConsistentChromaPalette} bars={[hslColorBar]} />
 		<hr />
 		<HslChromaDiscrepancies />
 	</React.StrictMode>
